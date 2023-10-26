f7ec0b1e1b87a02f577f53c95506596859b68e4d
KKTiw <149063706+KKTiw@users.noreply.github.com>
2023-10-26 06:55:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.title('My parents new healthy diner menu')
 streamlit.text('My parents new healthy')
-streamlit.web('My parents new healthy')
+streamlit.header('My parents new healthy')
