6508f93ed52f31c8ecb98796d413b10f8dac1a22
KKTiw <149063706+KKTiw@users.noreply.github.com>
2023-10-29 16:42:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,18 +20,15 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice fruit advice!')
-try:
-  fruit_choice=streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-     streamlit.error("Pls select a fruit to get information")
-  else:
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-    fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-def get_fruityvice_data(this_fruit_choice):
+
+ def get_fruityvice_data(this_fruit_choice):
   fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+return fruityvice_normalized
+streamlit.header('Fruityvice fruit advice!')
+try:
+ fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice) 
+  fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 return fruityvice_normalized
 
 
