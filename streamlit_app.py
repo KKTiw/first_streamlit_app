c0ae00afd547e47e131393fcc9ceae7fd52fc969
KKTiw <149063706+KKTiw@users.noreply.github.com>
2023-10-29 16:10:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,10 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice fruit advice')
-fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered',fruit_choice)
-streamlit.header('Fruityvice fruit advice')
-try:
-  fruit_choice=streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Pls select a fruit to get information")
-  else:
-    fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+import requests
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+fruity_normalized=pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruity_normalized)
+
+
 
