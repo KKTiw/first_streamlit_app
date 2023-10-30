3624d8ac64a3541a43cddbda32803d7ca871ff69
KKTiw <149063706+KKTiw@users.noreply.github.com>
2023-10-30 12:36:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice fruit advice!')
 fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choice)
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())    
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+#fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())    
+#streamlit.dataframe(fruityvice_normalized)
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())    
@@ -34,6 +34,10 @@ streamlit.header('Fruityvice fruit advice!')
 try:
    fruit_choice=streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information")            
+        streamlit.error("Please select a fruit to get information")
+   else:
+       back_from_function=get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
+       
 except URLError as e:
     streamlit.error()
