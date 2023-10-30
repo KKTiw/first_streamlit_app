f2634a199a17e4e202a9b4c68b4ed8ee7cefe265
KKTiw <149063706+KKTiw@users.noreply.github.com>
2023-10-30 10:29:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,8 @@ try:
             streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
                     def get_fruityvice_data(this_fruit_choice):
-                                                             fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-                                                             fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+                     fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+                     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
 return fruityvice_normalized
      
 streamlit.header('Fruityvice fruit advice!')
