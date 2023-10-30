d9408b452fc3ac31923288141581d7a1881c7681
KKTiw <149063706+KKTiw@users.noreply.github.com>
2023-10-30 11:14:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,8 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice fruit advice!')
+
+streamlit.stop()
 def get_fruityvice_data(this_fruit_choice):
      fruityvice_response=requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
